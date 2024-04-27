100e25abe800d96c92c5105a55f84e678db4b0b1
VincentCode <vincent.bauduin2007@gmail.com>
2024-04-27 20:51:59 +0200

    Add Mongoose and connect to database

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 // Import
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose')
 
 // Setup
 const app = express();
 app.set('view engine', 'ejs');
 
+// Database Setup
+const dbURI = 'mongodb+srv://kerlookAdmin:look158ker@kerlookcluster.ku0i3sg.mongodb.net/?retryWrites=true&w=majority&appName=kerlookCluster'
+mongoose.connect(dbURI)
+    .then((result) => {
+        console.log('connected to DB');
+        app.listen(3000);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+    
 // Middleware & static files 
 app.use(express.static('public'));
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: true}));
-
-// TEMPORARY
-app.listen(3000);
 
 // Request
 app.get('/', (req, res) => {
