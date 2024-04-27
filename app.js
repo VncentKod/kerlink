ec5f3c10fb1c5c08dfb954366975a93291a47eff
VincentCode <vincent.bauduin2007@gmail.com>
2024-04-27 22:34:58 +0200

    Make The Logic of the admin page

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const morgan = require('morgan');
 const mongoose = require('mongoose')
 
 // Internal import
-const FormClient = require('./models/formClientModel')
+const FormClient = require('./models/formClientModel');
+const Users = require('./models/usersModel');
 
 // Setup
 const app = express();
@@ -26,6 +27,7 @@ mongoose.connect(dbURI)
 app.use(express.static('public'));
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: true}));
+let connectedAsAdmin = false;
 
 // Request
 app.get('/', (req, res) => {
@@ -38,7 +40,31 @@ app.post('/', (req, res) => {
         .then((result) => {
             res.render('done');
         });
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
 })
+
+app.get('/admin', (req, res, next) => {
+    if(connectedAsAdmin) {
+        res.render('done')
+    } else {
+        res.redirect('login');
+    }
+})
+
+app.post('/login', (req, res) => {
+    Users.findById('662d5a472cd3c07bd5a0347e')
+        .then((result) => {
+            if (result.login === req.body.login && result.password === req.body.password) {
+                console.log(result.login);
+                console.log(req.body.login);
+                connectedAsAdmin = true;
+            }
+            res.redirect('/admin');
+        })
+});
 
 app.use((req, res) => {
     res.status(404).render('404');
